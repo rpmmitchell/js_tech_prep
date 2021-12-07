92bc7fe90b83c0367c669419b9b862ebe78d92f3
Ryan <rpmmitchell@outllok.com>
2021-12-07 14:04:59 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var preorderTraversal = function(root) {
+    if(!root) return [];
+    const stack = [root];
+    const result = [];
+    
+    while(stack.length){
+        let node = stack.pop();
+        
+        result.push(node.val);
+        if(node.right) stack.push(node.right);
+        if(node.left) stack.push(node.left);
+        
+    }
+    return result
+};
